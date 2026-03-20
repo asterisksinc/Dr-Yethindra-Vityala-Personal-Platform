ec6233034f9909937d9a71d82e59ea5ba501d23c
aniketh <anikethbusavale03@gmail.com>
2026-03-21 00:39:40 +0530

    feat: add initial Next.js root layout with global styles, fonts, and core components

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SmoothScroller>
-          <CursorTrail />
+          {/* <CursorTrail /> */}
           <VyNavbar />
           {children}
         </SmoothScroller>
